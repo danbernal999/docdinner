cc48a182b3629290b545d2ef05e20cd1b6ddee41
Brayan Daniel Bernal Lopez <bblopezbernal123@gmail.com>
2025-06-15 21:21:15 -0500

    Actualizacion Base de Datos

--- control_gastos.sql
+++ control_gastos.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 07-06-2025 a las 01:57:20
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-06-2025 a las 04:19:11
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Versión de PHP: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,10 +43,10 @@ CREATE TABLE `gastos_fijos` (
 --
 
 INSERT INTO `gastos_fijos` (`id`, `usuario_id`, `nombre_gasto`, `monto`, `fecha`, `categoria`, `descripcion`, `created_at`) VALUES
-(2, 1, 'Transporte', 100000.00, '2025-06-30', 'Transporte', 'Gasolina', '2025-05-25 00:20:35'),
-(3, 1, 'Internet', 50000.00, '2025-06-05', 'Hogar y Decoración', 'Claro', '2025-05-25 00:21:21'),
-(4, 1, 'Vivienda', 100000000.00, '2030-03-26', 'Vivienda', 'Apartamento', '2025-05-25 00:22:34'),
-(5, 2, 'Compensar', 200000.00, '2025-05-31', 'Salud y Belleza', 'Brackets', '2025-05-25 00:41:49');
+(5, 2, 'Compensar', 200000.00, '2025-05-31', 'Salud y Belleza', 'Brackets', '2025-05-25 00:41:49'),
+(9, 10, 'Transporte', 150000.00, '2025-06-30', 'Transporte', 'Gasolina', '2025-06-10 00:16:15'),
+(10, 10, 'Vivienda', 600000.00, '2025-07-01', 'Vivienda', 'Apartamento', '2025-06-13 01:50:39'),
+(11, 10, 'CityDent', 62000.00, '2025-06-15', 'Salud y Belleza', 'Brackets', '2025-06-13 01:51:19');
 
 -- --------------------------------------------------------
 
@@ -67,17 +67,9 @@ CREATE TABLE `historial_ahorros` (
 --
 
 INSERT INTO `historial_ahorros` (`id`, `meta_id`, `cantidad`, `fecha`, `descripcion1`) VALUES
-(3, 4, 3000000.00, '2025-05-26 15:56:40', ''),
-(4, 4, 3000000.00, '2025-05-26 16:18:11', ''),
-(5, 4, 3000000.00, '2025-05-26 16:18:23', ''),
-(6, 4, 3000000.00, '2025-05-26 16:20:18', ''),
-(7, 4, 4000000.00, '2025-05-26 16:20:34', ''),
-(10, 4, 2000000.00, '2025-05-26 21:54:23', ''),
-(11, 4, 3000000.00, '2025-05-26 22:21:08', ''),
-(13, 7, 2000000.00, '2025-05-26 22:42:12', ''),
-(14, 9, 3000000.00, '2025-05-26 22:44:28', ''),
-(15, 9, 1000000.00, '2025-05-26 23:03:18', ''),
-(16, 10, 500000.00, '2025-05-26 23:05:31', '');
+(20, 13, 50000.00, '2025-06-10 00:05:40', ''),
+(21, 14, 950000.00, '2025-06-10 00:11:00', ''),
+(22, 15, 80000.00, '2025-06-13 01:57:42', '');
 
 -- --------------------------------------------------------
 
@@ -103,12 +95,10 @@ CREATE TABLE `metas_ahorro` (
 
 INSERT INTO `metas_ahorro` (`id`, `usuario_id`, `nombre_meta`, `cantidad_meta`, `fecha_limite`, `descripcion`, `creada_en`, `cumplida`, `ahorrado`) VALUES
 (3, 2, 'Guayos', 200000.00, '2025-06-30', 'Croydon', '2025-05-25 01:07:42', 0, 0.00),
-(4, 1, 'Vivienda', 5000000.00, '2025-07-31', 'Penhouse', '2025-05-25 01:12:49', 0, 5000000.00),
 (5, 2, 'Xbox', 3000000.00, '2025-05-31', 'Xbox One', '2025-05-25 01:16:37', 0, 0.00),
-(7, 1, 'Moto', 10000000.00, '2025-05-28', 'Suzuki', '2025-05-26 22:42:03', 0, 2000000.00),
-(8, 1, 'Colchon', 600000.00, '2025-05-24', 'Comodísimos', '2025-05-26 22:43:45', 0, 0.00),
-(9, 1, 'Guayos', 4000000.00, '2025-05-31', 'Nike', '2025-05-26 22:44:14', 0, 4000000.00),
-(10, 1, 'Tatuajes', 1000000.00, '2025-05-29', 'RustyCry', '2025-05-26 22:47:39', 0, 500000.00);
+(13, 10, 'Zapatos Air', 200000.00, '2025-06-14', 'Nike Air', '2025-06-10 00:05:23', 0, 50000.00),
+(14, 10, 'Moto', 2000000.00, '2025-08-26', 'Akt 125', '2025-06-10 00:10:42', 0, 950000.00),
+(15, 10, 'Guayos', 150000.00, '2025-06-15', 'Nike', '2025-06-13 01:57:35', 0, 80000.00);
 
 -- --------------------------------------------------------
 
@@ -126,16 +116,17 @@ CREATE TABLE `usuarios` (
   `reset_token` varchar(255) DEFAULT NULL,
   `reset_expires` datetime DEFAULT NULL,
   `foto` varchar(255) DEFAULT NULL,
-  `auth_provider` varchar(50) DEFAULT 'local'
+  `auth_provider` varchar(50) DEFAULT 'local',
+  `saldo_inicial` decimal(15,2) DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `password`, `fecha_registro`, `ultimo_login`, `reset_token`, `reset_expires`, `foto`, `auth_provider`) VALUES
-(1, 'Brayan Daniel', 'bblopezbenal123@gmail.com', '$2y$10$WW42vzq8pV6xOUyLthKd2OUXa7/ohTGcEMz4tHddbo3ayeODSo8jy', '2025-05-24 21:22:24', NULL, NULL, NULL, NULL, 'local'),
-(2, 'draoky999', 'draoky@gmail.com', '$2y$10$BmJBKLB7BIpjJSx18sgj9uJFWbIEtkGfe0jCX3GerE2QphG6k8PqS', '2025-05-25 00:30:24', NULL, NULL, NULL, NULL, 'local');
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `password`, `fecha_registro`, `ultimo_login`, `reset_token`, `reset_expires`, `foto`, `auth_provider`, `saldo_inicial`) VALUES
+(2, 'draoky999', 'draoky@gmail.com', '$2y$10$BmJBKLB7BIpjJSx18sgj9uJFWbIEtkGfe0jCX3GerE2QphG6k8PqS', '2025-05-25 00:30:24', NULL, NULL, NULL, NULL, 'local', 0.00),
+(10, 'Brayan Bernal', 'bblopezbernal123@gmail.com', '', '2025-06-09 23:20:43', NULL, NULL, NULL, 'uploads/perfil_684b568864d6a.jpg', 'google', 700000.00);
 
 --
 -- Índices para tablas volcadas
@@ -177,25 +168,25 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `gastos_fijos`
 --
 ALTER TABLE `gastos_fijos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `historial_ahorros`
 --
 ALTER TABLE `historial_ahorros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `metas_ahorro`
 --
 ALTER TABLE `metas_ahorro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
@@ -223,3 +214,4 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
